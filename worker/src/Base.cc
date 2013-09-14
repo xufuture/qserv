d70cb39a139abca1a371ea5975e2c63de1668fdc
Daniel L. Wang <danielw@slac.stanford.edu>
2013-09-13 18:21:14 -0700

    worker: disable SelfOverlap generation

--- worker/src/Base.cc
+++ worker/src/Base.cc
@@ -74,9 +74,9 @@ std::string qWorker::CREATE_SUBCHUNK_SCRIPT =
     "CREATE DATABASE IF NOT EXISTS Subchunks_%1%_%4%;"
     "CREATE TABLE IF NOT EXISTS Subchunks_%1%_%4%.%2%_%4%_%5% ENGINE = MEMORY "
     "AS SELECT * FROM %1%.%2%_%4% WHERE %3% = %5%;"
-    "CREATE TABLE IF NOT EXISTS Subchunks_%1%_%4%.%2%SelfOverlap_%4%_%5% "
-    "ENGINE = MEMORY "
-    "AS SELECT * FROM %1%.%2%SelfOverlap_%4% WHERE %3% = %5%;"
+//    "CREATE TABLE IF NOT EXISTS Subchunks_%1%_%4%.%2%SelfOverlap_%4%_%5% "
+//    "ENGINE = MEMORY "
+//    "AS SELECT * FROM %1%.%2%SelfOverlap_%4% WHERE %3% = %5%;"
     "CREATE TABLE IF NOT EXISTS Subchunks_%1%_%4%.%2%FullOverlap_%4%_%5% "
     "ENGINE = MEMORY "
     "AS SELECT * FROM %1%.%2%FullOverlap_%4% WHERE %3% = %5%;"
@@ -89,7 +89,7 @@ std::string qWorker::CREATE_SUBCHUNK_SCRIPT =
 // %4% subChunkId (e.g., 34)
 std::string qWorker::CLEANUP_SUBCHUNK_SCRIPT =
     "DROP TABLE IF EXISTS Subchunks_%1%_%3%.%2%_%3%_%4%;"
-    "DROP TABLE IF EXISTS Subchunks_%1%_%3%.%2%SelfOverlap_%3%_%4%;"
+//    "DROP TABLE IF EXISTS Subchunks_%1%_%3%.%2%SelfOverlap_%3%_%4%;"
     "DROP TABLE IF EXISTS Subchunks_%1%_%3%.%2%FullOverlap_%3%_%4%;"
     ;
 
