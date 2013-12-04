1592b7d2c1a2e06926ca3e9f7d1766e799e66051
Jacek Becla <becla@slac.stanford.edu>
2013-12-04 14:53:36 -0800

    create db like implemented, added test

--- css/qserv_admin_impl.py
+++ css/qserv_admin_impl.py
@@ -41,12 +41,14 @@ class QservAdminImpl(object):
     def __init__(self):
         self._iFace = CssIFace()
 
-    # ---------------------------------------------------------------------------
+    #############################################################################
+    #### createDb
+    #############################################################################
     def createDb(self, dbName, options):
         """Creates database (options specified explicitly)."""
 
         if self._dbExists(dbName):
-            print "ERROR: db already exists"
+            print "ERROR: db '%s' already exists" % dbName
             return ERROR
         try:
             dbP = "/DATABASES/%s" % dbName
@@ -60,37 +62,48 @@ class QservAdminImpl(object):
             self._iFace.create("%s/partitioningId" % dbP, str(pId))
             self._iFace.create("%s/releaseStatus" % dbP,"UNRELEASED")
             self._iFace.create("%s/objIdIndex" % dbP, options["objectIdIndex"])
-            self._iFace.create("%s/LOCK/comments" % dbP)
-            self._iFace.create("%s/LOCK/estimatedDuration" % dbP)
-            self._iFace.create("%s/LOCK/lockedBy" % dbP)
-            self._iFace.create("%s/LOCK/lockedTime" % dbP)
-            self._iFace.create("%s/LOCK/mode" % dbP)
-            self._iFace.create("%s/LOCK/reason" % dbP)
+            self._createDbLockSection(dbP)
         except CssException as e:
             print e.getErrMsg()
             return ERROR
         return SUCCESS
 
-    # ---------------------------------------------------------------------------
+    #############################################################################
+    #### createDbLike
+    #############################################################################
     def createDbLike(self, dbName, dbName2):
         """Creates database using an existing database as a template."""
         print "Creating db '%s' like '%s'" % (dbName, dbName2)
-        print "createDbLike impl not finished"
-        # need to create all other things in CSS related to that db
+        if self._dbExists(dbName):
+            print "ERROR: db '%s' already exists" % dbName
+            return ERROR
+        if not self._dbExists(dbName2):
+            print "ERROR: db '%s' does not exist." % dbName2
+            return ERROR
+        try:
+            self._copyKeyValue(dbName, dbName2, 
+                               ("dbGroup", "partitioningId", 
+                                "releaseStatus", "objIdIndex"))
+        except CssException as e:
+            print e.getErrMsg()
+            return ERROR
+        self._createDbLockSection("/DATABASES/%s" % dbName)
         return SUCCESS
 
-    # ---------------------------------------------------------------------------
+    #############################################################################
+    #### dropDb
+    #############################################################################
     def dropDb(self, dbName):
         """Drops database."""
         if not self._dbExists(dbName):
             print "ERROR: db does not exist"
             return ERROR
-        self._iFace.delete("/DATABASES/%s" % dbName)
-        print "createDb impl not finished"
-        # need to delete all other things in CSS related to that db
+        self._iFace.delete("/DATABASES/%s" % dbName, recursive=True)
         return SUCCESS
 
-    # ---------------------------------------------------------------------------
+    #############################################################################
+    #### showDatabases
+    #############################################################################
     def showDatabases(self):
         """Shows list of databases registered for Qserv use."""
         if not self._iFace.exists("/DATABASES"):
@@ -98,19 +111,48 @@ class QservAdminImpl(object):
         else:
             print self._iFace.getChildren("/DATABASES")
 
-    # ---------------------------------------------------------------------------
+    #############################################################################
+    #### showEverything
+    #############################################################################
     def showEverything(self):
         """Dumps entire metadata in CSS to stdout. Very useful for debugging."""
         self._iFace.printAll()
 
-    # ---------------------------------------------------------------------------
+    #############################################################################
+    #### dropEverything
+    #############################################################################
     def dropEverything(self):
         """Deletes everything from the CSS (very dangerous, very useful for
         debugging."""
         self._iFace.deleteAll("/")
 
-    # ---------------------------------------------------------------------------
+    #############################################################################
+    #### _dbExists
+    #############################################################################
     def _dbExists(self, dbName):
         """Checks if the database exists."""
         p = "/DATABASES/%s" % dbName
         return self._iFace.exists(p)
+
+    #############################################################################
+    #### _copyKeyValue
+    #############################################################################
+    def _copyKeyValue(self, dbDest, dbSrc, theList):
+        """It copies items specified in theList from dbSrc to dbDest."""
+        dbS  = "/DATABASES/%s" % dbSrc
+        dbD = "/DATABASES/%s" % dbDest
+        for x in theList:
+            v = self._iFace.get("%s/%s" % (dbS, x))
+            self._iFace.create("%s/%s" % (dbD, x), v)
+
+    #############################################################################
+    #### _createDbLockSection
+    #############################################################################
+    def _createDbLockSection(self, dbP):
+        """It creates key/values related to "LOCK" for a given db path."""
+        self._iFace.create("%s/LOCK/comments" % dbP)
+        self._iFace.create("%s/LOCK/estimatedDuration" % dbP)
+        self._iFace.create("%s/LOCK/lockedBy" % dbP)
+        self._iFace.create("%s/LOCK/lockedTime" % dbP)
+        self._iFace.create("%s/LOCK/mode" % dbP)
+        self._iFace.create("%s/LOCK/reason" % dbP)
