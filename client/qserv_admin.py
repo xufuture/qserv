6f110498fa132f4b79846237d979811d97ad5472
Jacek Becla <becla@slac.stanford.edu>
2013-12-20 14:30:32 -0800

    simplified logger naming

--- client/qserv_admin.py
+++ client/qserv_admin.py
@@ -28,6 +28,11 @@ corresponding function.
 @author  Jacek Becla, SLAC
 
 Known issues and todos:
+ - !!!!!! 
+
+need to catch exceptions from impl and handle them correctly!!!!!!!!
+
+
  - deal with user authentication
  - many commands still need to be implemented
  - need to separate dangerous admin commands like DROP EVERYTHING
