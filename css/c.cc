7137a2791641b67fc34476a5eba3fd2bcb13a117
Jacek Becla <becla@slac.stanford.edu>
2014-02-10 14:04:43 -0800

    more work on the c cssInterface

--- css/c.cc
+++ css/c.cc
@@ -29,11 +29,12 @@ CssInterface::CssInterface(string const& connInfo) {
 }
 
 CssInterface::~CssInterface() {
+    zookeeper_close(_zh);
 }
 
 void
 CssInterface::create(string const& key, string const& value) {
-    cout << "*** create " << key << " --> " << value << endl;
+    cout << "*** CssInterface::create(), " << key << " --> " << value << endl;
     char buffer[512];
     int rc = zoo_create(_zh, key.c_str(), value.c_str(), value.length(), 
                         &ZOO_OPEN_ACL_UNSAFE, ZOO_EPHEMERAL,
@@ -45,17 +46,17 @@ CssInterface::create(string const& key, string const& value) {
     }
 }
 
-string
+bool
 CssInterface::exists(string const& key) {
-    cout << "*** exist " << key << endl;
-    string s;
-    throw("exist not implemented");
-    return s;
+    cout << "*** CssInterface::exist(), key: " << key << endl;
+    struct Stat stat;
+    int rc = zoo_exists(_zh, key.c_str(), 0,  &stat);
+    return rc == 0;
 }
 
 string
 CssInterface::get(string const& key) {
-    cout << "*** get " << key << endl;
+    cout << "*** CssInterface::get(), key: " << key << endl;
     char buffer[512];
     memset(buffer, 0, 512);
     int buflen = sizeof(buffer);
@@ -72,19 +73,32 @@ CssInterface::get(string const& key) {
 
 std::vector<string> 
 CssInterface::getChildren(string const& key) {
-    // see example at:
-    // http://svn.apache.org/repos/asf/zookeeper/tags/release-3.3.4/src/c/src/cli.c
+    cout << "*** CssInterface::getChildren, key: " << key << endl;
+    struct String_vector strings;
+    int rc = zoo_get_children(_zh, key.c_str(), 0, &strings);
+    if ( rc ) {
+        throw("Key not found");
+    }
+    cout << "got " << strings.count << " children" << endl;
     vector<string> v;
-    throw("getChildren not implemented");
+    int i;
+    for (i=0 ; i < strings.count ; i++) {
+        cout << "   " << i+1 << ": " << strings.data[i] << endl;
+        v.push_back(strings.data[i]);
+    }
     return v;
 }
 
 void
-CssInterface::deleteNode(string const& key, bool recurvive) {
-    zookeeper_close(_zh);
+CssInterface::deleteNode(string const& key) {
+    int rc = zoo_delete(_zh, key.c_str(), -1);
 }
 
 ////////////////////////////////////////////////////////////////////////////////////
+
+void x(CssInterface& cssI, string k) {
+}
+
 int main(int argc, char* argv[]) {
     CssInterface cssI = CssInterface("localhost:2181");
 
@@ -98,6 +112,29 @@ int main(int argc, char* argv[]) {
 
     string s = cssI.get(k1);
     cout << "got " << s << " for key " << k1 << endl;
+
+    cout << "checking exist for " << "/xyzA" << endl;
+    cout << "got: " << cssI.exists("/xyzA") << endl;
+
+    cout << "checking exist for " << "/xyzC" << endl;
+    cout << "got: " << cssI.exists("/xyzC") << endl;
+
+    std::vector<string> v = cssI.getChildren("/");
+    cout << "children of '/': ";
+    for (std::vector<string>::iterator it = v.begin() ; it != v.end(); ++it) {
+        cout << *it << " ";
+    }
+    cout << endl;
+
+    cssI.deleteNode(k1);
+    cssI.deleteNode(k2);
+
+    v = cssI.getChildren("/");
+    cout << "children of '/': ";
+    for (std::vector<string>::iterator it = v.begin() ; it != v.end(); ++it) {
+        cout << *it << " ";
+    }
+    cout << endl;
 
     return 0;
 }
