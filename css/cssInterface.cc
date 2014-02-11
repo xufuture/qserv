cc479db35375dd60cb8d4045e7357bdf609ab692
Jacek Becla <becla@slac.stanford.edu>
2014-02-11 11:36:32 -0800

    c renamed to cssInterface renamed

--- css/cssInterface.cc
+++ css/cssInterface.cc
@@ -1,14 +1,42 @@
+/*
+ * LSST Data Management System
+ * Copyright 2014 LSST Corporation.
+ *
+ * This product includes software developed by the
+ * LSST Project (http://www.lsst.org/).
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the LSST License Statement and
+ * the GNU General Public License along with this program.  If not,
+ * see <http://www.lsstcorp.org/LegalNotices/>.
+ */
 
-// First implementation of the CssInterface, based on:
-//  http://zookeeper.apache.org/doc/r3.3.4/zookeeperProgrammers.html#ZooKeeper+C+client+API
-//
-// To build:
-//   gcc css/c.cc -c -I <distDir>/include/zookeeper
-//   export LD_LIBRARY_PATH=<distDir>/lib
-//
-// To do:
-//  * logging
-//  * perhaps switch to async (seems to be recommended by zookeeper)
+/**
+  * @file CssInterface.cc
+  *
+  * @brief Interface to the Common State System.
+  *
+  * @Author Jacek Becla, SLAC
+  */
+
+/*
+ * Based on:
+ * http://zookeeper.apache.org/doc/r3.3.4/zookeeperProgrammers.html#ZooKeeper+C+client+API
+ *
+ * To do:
+ *  - logging
+ *  - perhaps switch to async (seems to be recommended by zookeeper)
+ */
+
 
 // standard library imports
 #include <iostream>
@@ -17,7 +45,7 @@
 #include <string.h> // for memset
 
 // local imports
-#include "c.h"
+#include "cssInterface.h"
 
 
 using std::cout;
