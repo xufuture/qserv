0f495b2bb91c3961d4496e5ebef5982bd6d757d9
Jacek Becla <becla@slac.stanford.edu>
2014-02-12 10:01:11 -0800

    added Store and unitest

---
+++ css/Store.h
@@ -0,0 +1,81 @@
+/*
+ * LSST Data Management System
+ * Copyright 2014 LSST Corporation.
+ *
+ * This product includes software developed by the
+ * LSST Project (http://www.lsst.org/).
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the LSST License Statement and
+ * the GNU General Public License along with this program.  If not,
+ * see <http://www.lsstcorp.org/LegalNotices/>.
+ */
+
+/**
+  * @file Store.h
+  *
+  * @brief A store that manage information from Central State System for Qserv.
+  *
+  * @Author Jacek Becla, SLAC
+  */
+
+#ifndef LSST_QSERV_CSS_STORE_H
+#define LSST_QSERV_CSS_STORE_H
+
+// Standard
+#include <iostream>
+#include <map>
+#include <string>
+#include <vector>
+
+// Boost
+#include <boost/shared_ptr.hpp>
+#include <boost/thread.hpp>     // for mutex
+
+// Local imports
+#include "cssInterface.h"
+
+namespace lsst {
+namespace qserv {
+namespace css {
+
+class CssInterface; // forward declaration
+    
+/** The class stores Qserv-specific metadata and state information from the
+    Central State System.
+ */
+class Store {
+public:
+    Store(std::string const&, std::string const&);
+    ~Store();
+
+    // accessors
+    bool checkIfContainsDb(std::string const&);
+    bool checkIfContainsTable(std::string const&, std::string const&);
+    bool checkIfTableIsChunked(std::string const&, std::string const&);
+    bool checkIfTableIsSubChunked(std::string const&, std::string const&);
+    std::vector<std::string> getAllowedDbs();
+    std::vector<std::string> getChunkedTables(std::string const&);
+    std::vector<std::string> getSubChunkedTables(std::string const&);
+    std::vector<std::string> getPartitionCols(std::string const&, 
+                                              std::string const&);
+    int getChunkLevel(std::string const& db, std::string const& table);
+    std::string getKeyColumn(std::string const& db, std::string const& table);
+
+private:
+    lsst::qserv::css::CssInterface* _cssI;
+    std::string _prefix; // optional prefix, for isolating tests from production
+};
+
+}}} // namespace lsst::qserv::css
+
+#endif // LSST_QSERV_CSS_STORE_H
