880d61494afa4dfc18f5bac7e9a70ced275f9514
Jacek Becla <becla@slac.stanford.edu>
2014-02-12 14:08:51 -0800

    added cssException class and unittest

---
+++ css/cssException.cc
@@ -0,0 +1,66 @@
+/*
+ * LSST Data Management System
+ * Copyright 2014 LSST Corporation.
+ *
+ * This product includes software developed by the
+ * LSST Project (http://www.lsst.org/).
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the LSST License Statement and
+ * the GNU General Public License along with this program.  If not,
+ * see <http://www.lsstcorp.org/LegalNotices/>.
+ */
+
+/**
+  * @file CssException.h
+  *
+  * @brief Exception class for CssInterface.
+  *
+  * @Author Jacek Becla, SLAC
+  */
+
+
+// Standard library
+#include <iostream>
+
+// Boost includes
+#include "boost/assign.hpp"
+
+// Local includes
+#include "cssException.h"
+
+using std::map;
+using std::string;
+
+namespace qCss = lsst::qserv::css;
+
+map<qCss::CssException::errCodeT, string> 
+qCss::CssException::_errMap = boost::assign::map_list_of 
+    (DB_DOES_NOT_EXIST,  "Database does not exist")
+    (KEY_DOES_NOT_EXIST, "Key does not exist" )
+    (TB_DOES_NOT_EXIST, "Table does not exist");
+
+qCss::CssException::CssException(errCodeT errCode, std::string const& extraMsg) :
+    std::runtime_error(""),
+    _errCode(errCode) {
+    string s = "CssException: " + _errMap.find(_errCode)->second;
+    if (extraMsg != "") {
+        s = s + " (" + extraMsg + ")";
+    }
+    _errMsg = new char[s.length()+1];
+    strcpy(_errMsg, s.c_str());
+}
+
+const char*
+qCss::CssException::what() const throw () {
+    return _errMsg;
+}
