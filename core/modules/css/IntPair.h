126ce5e3bbfd3fba8aca77ba0cfaa6682aaf1993
Jacek Becla <becla@slac.stanford.edu>
2014-03-06 22:43:24 -0800

    added file I should have added in previous commit

---
+++ core/modules/css/IntPair.h
@@ -0,0 +1,45 @@
+/*
+ * LSST Data Management System
+ * Copyright 2014 LSST Corporation.
+ *
+ * This product includes software developed by the
+ * LSST Project (http://www.lsst.org/).
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the LSST License Statement and
+ * the GNU General Public License along with this program.  If not,
+ * see <http://www.lsstcorp.org/LegalNotices/>.
+ */
+
+/**
+  * @file IntPair.h
+  *
+  * @brief Struct containing 2 integers, for C++-->python swig.
+  *
+  * @Author Jacek Becla, SLAC
+  */
+
+#ifndef LSST_QSERV_CSS_INTPAIR_H
+#define LSST_QSERV_CSS_INTPAIR_H
+
+namespace lsst {
+namespace qserv {
+namespace master {
+
+struct IntPair {
+    int a;
+    int b;
+};
+
+}}} // namespace lsst::qserv::master
+
+#endif // LSST_QSERV_CSS_INTPAIR_H
