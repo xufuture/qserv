e1050f63dd50b28d4d2f090bf2a0212e33be9140
Jacek Becla <becla@slac.stanford.edu>
2014-03-25 11:07:15 -0700

    comment fixed

--- client/qserv_admin.py
+++ client/qserv_admin.py
@@ -27,13 +27,7 @@ corresponding function.
 
 @author  Jacek Becla, SLAC
 
-
 Known issues and todos:
- - !!!!!! 
-
-need to catch exceptions from impl and handle them correctly!!!!!!!!
-
-
  - deal with user authentication
  - many commands still need to be implemented
  - need to separate dangerous admin commands like DROP EVERYTHING
