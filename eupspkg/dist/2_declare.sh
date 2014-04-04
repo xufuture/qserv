4c3d5dcbddd4d3271deb7623cd8c71d80e79cf8f
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2014-04-04 14:20:35 +0200

    eups package is now done from Qserv git directory (and no more TaP)

--- eupspkg/dist/2_declare.sh
+++ eupspkg/dist/2_declare.sh
@@ -26,7 +26,7 @@ rm -rf LOCAL_PKGROOT/*
 eups declare python system -r none -m none
 eups declare java system -r none -m none
 eups declare numpy system -r none -m none
-eups_dist mysql 5.1.65 && 
+eups_dist mysql 5.1.65 &&
 eups_dist xrootd qs5 &&
 eups_dist lua 5.1.4 &&
 eups_dist luasocket 2.0.2 &&
@@ -35,15 +35,14 @@ eups_dist luaexpat 1.1 &&
 eups_dist luaxmlrpc v1.2.1-2 &&
 eups_dist libevent 2.0.16-stable &&
 eups_dist mysqlproxy 0.8.2 &&
-eups_dist virtualenv_python 1.10.1 && 
+eups_dist virtualenv_python 1.10.1 &&
 eups_dist mysqlpython 1.2.3 &&
-eups_dist protobuf 2.4.1 && 
-eups_dist zopeinterface 3.8.0 && 
-eups_dist twisted 12.0.0 && 
-eups_dist zookeeper 3.4.6 && 
-eups_dist db 1.0 ${REPOSITORY_BASE_DMS} && 
-eups_dist kazoo 1.3.1 && 
-eups_dist qserv ${VERSION} ||
+eups_dist protobuf 2.4.1 &&
+eups_dist zopeinterface 3.8.0 &&
+eups_dist twisted 12.0.0 &&
+eups_dist zookeeper 3.4.6 &&
+eups_dist db 1.0 ${REPOSITORY_BASE_DMS} &&
+eups_dist kazoo 1.3.1 ||
 {
     echo "ERROR : unable to create all packages"
     exit 2
--- eupspkg/functions.sh
+++ eupspkg/functions.sh
@@ -38,11 +38,11 @@ eups_install() {
 
 eups_dist() {
     if [ -z "$1" -o -z "$2" ]; then
-        echo "eups_dist requires two arguments"
+        echo "eups_dist requires at least two arguments"
         exit 1
     fi
-    product=$1 &&
-    version=$2
+    local product=$1 &&
+    local version=$2
 
     if [ -z "$3" ]; then
         gitrepo=${REPOSITORY_BASE_CONTRIB}
@@ -58,14 +58,25 @@ eups_dist() {
     git clone ${gitrepo}/${product} &&
     cd ${product} &&
     git checkout -q ${version} -- ups &&
+    eups_dist_create $product $version
+}
+
+eups_dist_create () {
+    if [ -z "$1" -o -z "$2" ]; then
+        echo "eups_dist_create requires two arguments"
+        exit 1
+    fi
+    local product=$1 &&
+    local version=$2
     cmd="eups declare ${product} ${version} -r ." &&
     echo "CMD : $cmd" &&
     $cmd &&
-    cmd="eups distrib create --nodepend --server-dir=${LOCAL_PKGROOT} -f generic -d eupspkg -t current ${product}"
+    cmd="eups distrib create --nodepend --server-dir=${LOCAL_PKGROOT} -f generic -d eupspkg -t current ${product} ${version}"
     echo "Running : $cmd" &&
     $cmd &&
     # for debug purpose only : build file generation
-    # eups expandbuild -V ${version} ups/${product}.build > ${product}-${version}.build
+    # eups expandbuild -V ${version} ups/${product}.build >
+    # ${product}-${version}.build
     cd ${CWD} ||
     {
         echo "ERROR : while creating package $product, $version"
