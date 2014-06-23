71095f02d5e79edd58a1129208443e350a11a2d4
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2014-06-23 18:11:03 +0200

    init.d scripts now manage QSERV_RUN_DIR

--- admin/bin/qserv-configure.py
+++ admin/bin/qserv-configure.py
@@ -16,10 +16,12 @@ def parseArgs():
             )
 
     #
-    step_list = ['prep','run']
-    step_option_values = step_list + ['all']
+    step_option_values = configure.STEP_LIST
+    components = ['mysql', 'xrootd', 'qserv-czar', 'scisql']
+    configure.STEP_RUN_LIST = ['read-meta-cfg', 'mkdirs', 'etc'] + components + ['client']
+    step_list = ['prep'] + configure.STEP_RUN_LIST
+    step_option_values = step_list
     parser.add_argument("-s", "--step", dest="step", choices=step_option_values,
-        default='all',
         help="Qserv configurator install step : \n'" +
         "     " + step_option_values[0] + " : create a qserv_run_dir " +
         "directory which will contains configuration and execution data " +
@@ -27,7 +29,13 @@ def parseArgs():
         "     " + step_option_values[1] + " : fill qserv_run_dir " +
         "configuration files with values from meta-config file qserv.conf"
         )
+    
+    parser.add_argument("-a", "--all", dest="all", action='store_true',
+            default=False,
+            help="Clean execution directory and then run all configuration steps"
+            )
 
+    # Logging management
     verbose_dict = {
         'DEBUG'     : logging.DEBUG,
         'INFO'      : logging.INFO,
@@ -40,6 +48,12 @@ def parseArgs():
         default='INFO',
         help="Verbosity level"
         )
+
+    # forcing options which may ask user confirmation
+    parser.add_argument("-f", "--force", dest="force", action='store_true',
+            default=False,
+            help="Forcing removal of existing execution data"
+            )
 
     # run dir, all mutable data related to a qserv running instance are
     # located here
@@ -62,10 +76,12 @@ def parseArgs():
 
     args = parser.parse_args()
 
-    if args.step=='all':
-        args.step_list = step_list
-    else:
+    if args.step is not None and len(args.step) != 0 :
         args.step_list = [args.step]
+    elif args.all:
+        args.step_list = configure.STEP_LIST
+    else:
+        args.step_list = configure.STEP_RUN_LIST
 
     args.verbose_level = verbose_dict[args.verbose_str]
 
@@ -80,13 +96,11 @@ def main():
 
     args = parseArgs()
 
-    #logging.basicConfig(level=logging.INFO)
-    logging.basicConfig(level=args.verbose_level)
+    logging.basicConfig(format='%(levelname)s: %(message)s', level=args.verbose_level)
 
-    logging.info("Qserv configuration tool"+
+    logging.info("Qserv configuration tool\n"+
         "======================================================================="
     )
-
 
     qserv_dir = os.path.abspath(
                     os.path.join(
@@ -101,37 +115,48 @@ def main():
             .format(template_config_dir, args.qserv_run_dir)
         )
 
-        if configure.user_yes_no_query(
-            "WARNING : Do you want to erase all configuration" +
-            " data in {0} ?".format(args.qserv_run_dir)
-        ):
-            copy_and_overwrite(template_config_dir, args.qserv_run_dir)
-        else:
-            print
+        if not args.force :
+            if configure.user_yes_no_query(
+                "WARNING : Do you want to erase all configuration" +
+                " data in {0} ?".format(args.qserv_run_dir)
+            ):
+                copy_and_overwrite(template_config_dir, args.qserv_run_dir)
+            else:
+                logging.info("Stopping Qserv configuration, please specify an other configuration directory")
+                sys.exit(1)
 
         for line in fileinput.input(args.meta_config_file, inplace = 1):
             print line.replace("run_base_dir =", "run_base_dir = " + args.qserv_run_dir),
 
-    if 'run' in args.step_list:
+    def intersect(l1, l2):
+        l = []
+        for elem in l2 :
+            if elem in l1 :
+                l.append(elem)
+        return l
 
-        #########################
-        #
-        # Reading config file
-        #
-        #########################
+    def contains_configuration_step(step_list):
+        return (len(intersect(step_list, configure.STEP_RUN_LIST)) > 0)
+    
+
+    ###################################
+    #
+    # Running configuration procedure
+    #
+    ###################################
+    if  contains_configuration_step(args.step_list):
+
         try:
+            logging.info("Reading meta-configuration file")
             config = commons.read_config(args.meta_config_file)
         except ConfigParser.NoOptionError, exc:
             logging.fatal("An option is missing in your configuration file: %s" % exc)
             sys.exit(1)
 
-        #####################################
-        #
-        # Defining main directory structure
-        #
-        #####################################
-        configure.check_root_dirs()
-        configure.check_root_symlinks()
+        if 'mkdirs' in args.step_list:
+            logging.info("Defining main directory structure")
+            configure.check_root_dirs()
+            configure.check_root_symlinks()
 
         ##########################################
         #
@@ -139,40 +164,42 @@ def main():
         # using templates and meta_config_file
         #
         ##########################################
-        template_root = os.path.join(config['qserv']['run_base_dir'],"templates", "server")
-        dest_root = os.path.join(config['qserv']['run_base_dir'])
-        configure.apply_templates(
-            template_root,
-            dest_root
-        )
+        if 'etc' in args.step_list:
+            logging.info(
+                "Creating configuration files in {0}".format(os.path.join(config['qserv']['run_base_dir'],"etc")) +
+                " and scripts in {0}".format(os.path.join(config['qserv']['run_base_dir'],"tmp"))
+            )
+            template_root = os.path.join(config['qserv']['run_base_dir'],"templates", "server")
+            dest_root = os.path.join(config['qserv']['run_base_dir'])
+            configure.apply_templates(
+                template_root,
+                dest_root
+            )
 
-        #########################
-        #
-        # Configuring services
-        #
-        #########################
-        configuration_scripts_dir=os.path.join(config['qserv']['run_base_dir'],'tmp','configure')
-        component_list = ['mysql', 'xrootd', 'qserv-czar']
+        components_to_configure = intersect(args.step_list, configure.COMPONENTS)
+        if len(components_to_configure) > 0 :
+            logging.info("Running configuration scripts")
+            configuration_scripts_dir=os.path.join(config['qserv']['run_base_dir'],'tmp','configure')
 
-        if config['qserv']['node_type'] in ['mono','worker']:
-           component_list.append('scisql')
+            if not config['qserv']['node_type'] in ['mono','worker']:
+                logging.info("Service isn't a worker or a mono-node instance : not configuring SciSQL")
+                component_to_configure.remove('scisql')
 
-        for c in component_list:
-            script = os.path.join( configuration_scripts_dir, c+".sh")
-            commons.run_command(script)
+            for c in components_to_configure:
+                script = os.path.join( configuration_scripts_dir, c+".sh")
+                commons.run_command(script)
 
-        #################################
-        #
-        # Creating client configuration
-        #
-        #################################
-        template_root = os.path.join(config['qserv']['run_base_dir'],"templates", "client")
-        homedir = os.path.expanduser("~")
-        dest_root = os.path.join(homedir, ".lsst")
-        configure.apply_templates(
-            template_root,
-            dest_root
-        )
+        if 'client' in args.step_list:
+            template_root = os.path.join(config['qserv']['run_base_dir'],"templates", "client")
+            homedir = os.path.expanduser("~")
+            dest_root = os.path.join(homedir, ".lsst")
+            logging.info(
+                "Creating client configuration file in {0}".format(dest_root)
+            )
+            configure.apply_templates(
+                template_root,
+                dest_root
+            )
 
 if __name__ == '__main__':
     main()
--- admin/bin/qserv-status.sh
+++ admin/bin/qserv-status.sh
@@ -1,10 +1,25 @@
 #!/bin/sh
 
-QSERV_RUN_DIR={{QSERV_RUN_DIR}}
-. ${QSERV_RUN_DIR}/bin/env.sh
+TMP_PATH=$(which qserv-configure.py)
+QSERV_BIN_DIR=$(dirname $TMP_PATH)
+source ${QSERV_BIN_DIR}/env.sh
+
+while getopts "hr:" option
+do
+    case $option in
+    h)
+        usage
+        ;;
+    r)
+        QSERV_RUN_DIR=${OPTARG}
+        ;;
+    esac
+done
+
+check_qserv_run_dir
 
 for service in ${SERVICES}; do
     ${QSERV_RUN_DIR}/etc/init.d/$service status
 done
 
- 
+
