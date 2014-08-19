447bb1c24d3cdfc812fa9f3c17c3a87b05b0da49
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2014-08-19 20:58:47 +0200

    Trying to retrieve lsstsw products

--- admin/tools/qserv-netdown.sh
+++ admin/tools/qserv-netdown.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-PKGROOT=${HOME}/tmp
+PKGROOT=/data/fjammes/distserver
 REMOTE_HOST=lsst-dev.ncsa.illinois.edu
 
 EUPS_VERSION=${EUPS_VERSION:-1.5.0}
@@ -9,14 +9,47 @@ EUPS_TARURL="https://github.com/RobertLuptonTheGood/eups/archive/$EUPS_TARBALL"
 
 EUPS_GITREPO=$"https://github.com/RobertLuptonTheGood/eups.git"
 
-cd ${PKGROOT}
-EUPS_PKGROOT="${PKGROOT}/eupspkg_root"
-EUPS_PKGROOT_QSERV="${PKGROOT}/eupspkg_root_qserv"
-rsync -ave ssh ${REMOTE_HOST}:/lsst/home/lsstsw/distserver/production/ ${EUPS_PKGROOT}
-rsync -ave ssh ${REMOTE_HOST}:/lsst/home/fjammes/public_html/qserv/ ${EUPS_PKGROOT_QSERV}
+if [ ! -d ${PKGROOT} ]; then
+    mkdir ${PKGROOT} ||
+    {
+        echo "Unable to create local distribution directory : ${PKGROOT}"
+        exit 1
+    }
 
-curl -L ${EUPS_TARURL} > ${EUPS_TARBALL} 
-git clone ${EUPS_GITREPO}
+fi
+cd ${PKGROOT} ||
+{
+    echo "Unable to go to local distribution directory directory : ${PKGROOT}"
+    exit 1
+}
+
+echo
+echo "Downloading eups tarball"
+echo "========================"
+echo
+curl -L ${EUPS_TARURL} > ${EUPS_TARBALL}
+
+echo
+echo "Downloading distribution server data on ${REMOTE_HOST}"
+echo "======================================================"
+echo
+EUPS_PKGROOT_QSERV="${PKGROOT}/eupspkg_root_qserv"
+rsync -ave ssh ${REMOTE_HOST}:/lsst/home/fjammes/public_html/qserv/ ${EUPS_PKGROOT_QSERV} ||
+{
+    echo "Unable to synchronize with Qserv distribution server on ${REMOTE_HOST}"
+    exit 1
+}
+
+echo
+echo "Downloading git repositories for Qserv and dependencies on ${REMOTE_HOST}"
+echo "========================================================================="
+echo
+GIT_REPOS="${PKGROOT}/git-repositories"
+rsync -ave ssh ${REMOTE_HOST}:/lsst/home/fjammes/src/lsstsw-release/build/${GIT_REPOS} ||
+{
+    echo "Unable to synchronize with git repositories on ${REMOTE_HOST}"
+    exit 1
+}
 
 EUPS_TARURL="file://${PKGROOT}/$EUPS_TARBALL"
 EUPS_GITREPO="${PKGROOT}/eups.git"
@@ -24,4 +57,4 @@ EUPS_GITREPO="${PKGROOT}/eups.git"
 echo "export EUPS_PKGROOT=${EUPS_PKGROOT}"
 echo "export EUPS_PKGROOT_QSERV=${EUPS_PKGROOT_QSERV}"
 echo "export EUPS_TARURL=${EUPS_TARURL}"
-echo "export EUPS_GITREPO=${EUPS_GITREPO}"
+echo "export EUPS_GIT<F6>REPO=${EUPS_GITREPO}"
