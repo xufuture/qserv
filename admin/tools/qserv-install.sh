92c8a5fec83525922a79e0b15e61d80650f1a1f7
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2014-08-21 01:10:41 +0200

    First working draft, need to clean and optimize

--- admin/tools/qserv-install.sh
+++ admin/tools/qserv-install.sh
@@ -19,10 +19,12 @@ STACK_DIR=$HOME/stack
 # WITHOUT NETWORK ACCESS
 #
 ########################
-EUPS_PKGROOT=${EUPS_PKGROOT:-"http://sw.lsstcorp.org/eupspkg"}
+# TODO remove ~fjammes distserver
+# EUPS_PKGROOT=${EUPS_PKGROOT:-"http://sw.lsstcorp.org/eupspkg"}
 # TODO if newinstall doesn't start with protocol:// add file://
 NEWINSTALL_URL="file://${EUPS_PKGROOT}/newinstall.sh"
-EUPS_PKGROOT_QSERV=${EUPS_PKGROOT_QSERV:-"http://lsst-web.ncsa.illinois.edu/~fjammes/qserv"}
+
+EUPS_PKGROOT_QSERV=${EUPS_PKGROOT:-"http://lsst-web.ncsa.illinois.edu/~fjammes/qserv"}
 
 if [ -d ${STACK_DIR} ]; then
     chmod -R 755 $STACK_DIR &&
@@ -40,16 +42,21 @@ cd $STACK_DIR ||
     exit 1
 }
 echo
-echo "Installing eups"
-echo "==============="
+echo "Installing LSST stack"
+echo "====================="
 echo
 curl -O ${NEWINSTALL_URL} ||
 {
     echo "Unable to download from ${NEWINSTALL_URL}"
     exit 1
 }
-time bash newinstall.sh
-. loadLSST.sh
+
+time bash newinstall.sh ||
+{
+    echo "newinstall.sh failed"
+    exit 1
+}
+source loadLSST.sh
 
 echo
 echo "Installing Qserv"
@@ -71,6 +78,9 @@ setup qserv_testdata ||
     echo "Unable to install Qserv test datasets"
     exit 1
 }
+
+
+rm ${GIT_CONFIG}
 
 echo
 echo "Configuring Qserv"
