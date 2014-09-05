acda7c640f97d67a7ac7e6b08f8a0d5901aaa3b5
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2014-09-05 11:06:27 +0200

    - updating dependencies list for ubuntu 14.04 - re-organizing documentation

--- admin/tools/prepare-install-from-internet-free-distserver.example.sh
+++ admin/tools/prepare-install-from-internet-free-distserver.example.sh
@@ -7,7 +7,8 @@ set -x
 # CUSTOMIZE NEXT PARAMETERS :
 #############################
 
-INTERNET_FREE_DISTSERVER_DIR=shared/dir/available/to/all/nodes
+INTERNET_FREE_DISTSERVER_PARENT_DIR=shared/dir/available/to/all/nodes
+INTERNET_FREE_DISTSERVER_DIR=${INTERNET_FREE_DISTSERVER_PARENT_DIR}/distserver
 
 # MODIFY PARAMETERS BELOW ONLY IF YOU KNOW WHAT YOU'RE DOING
 DISTSERVER_ARCHIVE_URL=http://lsst-web.ncsa.illinois.edu/~fjammes/qserv-internet-free/qserv-internet-free-distserver.tar.gz
@@ -20,13 +21,13 @@ TARBALL_DIR=${HOME}/tarballs
 mkdir -p ${TARBALL_DIR} 
 mkdir -p ${INTERNET_FREE_DISTSERVER_DIR} 
 rm -f ${TARBALL_DIR}/qserv-internet-free-distserver.tar.gz
-rm -rf ${INTERNET_FREE_DISTSERVER_DIR}/* 
+rm -rf ${INTERNET_FREE_DISTSERVER_DIR}
 
 # prepare internet-free distribution server
 cd ${TARBALL_DIR}
 curl -O ${DISTSERVER_ARCHIVE_URL}
-cd ${INTERNET_FREE_DISTSERVER_DIR} 
-tar zxvf ~/tarballs/qserv-internet-free-distserver.tar.gz --strip-components=1
+cd ${INTERNET_FREE_DISTSERVER_PARENT_DIR} 
+tar zxvf ${TARBALL_DIR}/qserv-internet-free-distserver.tar.gz
 
 # OPTIONAL : python 2.7 is required, if it isn't available on you system, and
 # if you can't install system packages, Anaconda may be a solution :
