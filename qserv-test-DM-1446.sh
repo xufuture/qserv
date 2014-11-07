061712455c43fa553248685d579d869c9616641e
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2014-11-07 14:32:06 -0800

    Integrate all test cases in test script

--- qserv-test-DM-1446.sh
+++ qserv-test-DM-1446.sh
@@ -1,3 +1,7 @@
+# require eups environment with Qserv stack and
+# setup -r .
+# setup -k -r qserv_testdata
+
 set -e
 set -x
 
@@ -7,19 +11,25 @@ killall mysqld xrootd mysql-proxy java python || echo "Nothing to kill"
 scons install
 qserv-configure.py --all --force
 
-# hack which moves qservMeta to czar DB
-# indeed this table in created by mono-node
-# integration test alongside MySQL data
-${QSERV_RUN_DIR}/bin/qserv-start.sh
-qserv-check-integration.py --load --case=01 || echo "Failed but OK"
-${QSERV_RUN_DIR}/bin/qserv-stop.sh
-cp -r ${QSERV_RUN_DIR}/var/lib/mysql-worker/qservMeta/ ${QSERV_RUN_DIR}/var/lib/mysql/
-# Remove qservMeta on the worker
-rm -rf  ${QSERV_RUN_DIR}/var/log/*
-mysqld_safe --defaults-file=${QSERV_RUN_DIR}/etc/my.worker.cnf >/dev/null 2>&1 &
-sleep 2
-mysql --no-defaults --socket="${QSERV_RUN_DIR}/var/lib/mysql-worker/mysql.sock" --user=root -pchangeme -e "DROP DATABASE qservMeta"
-kill `cat ${QSERV_RUN_DIR}/var/run/mysqld-worker/mysqld.pid`
+TEST_CASES="01 02 03"
+for case in $TEST_CASES
+do
+    ${QSERV_RUN_DIR}/bin/qserv-start.sh
+    # hack which moves qservMeta to czar DB
+    # indeed this table in created by mono-node
+    # integration test alongside MySQL data
+    mysql --no-defaults --socket="${QSERV_RUN_DIR}/var/lib/mysql/mysql.sock" --user=root -pchangeme -e "DROP DATABASE qservMeta"
+    qserv-check-integration.py --load --case=${case} || echo "Failed but OK"
+    ${QSERV_RUN_DIR}/bin/qserv-stop.sh
+    cp -r ${QSERV_RUN_DIR}/var/lib/mysql-worker/qservMeta/ ${QSERV_RUN_DIR}/var/lib/mysql/
+    # Remove qservMeta on the worker
+    rm -rf  ${QSERV_RUN_DIR}/var/log/*
+    mysqld_safe --defaults-file=${QSERV_RUN_DIR}/etc/my.worker.cnf >/dev/null 2>&1 &
+    sleep 2
+    mysql --no-defaults --socket="${QSERV_RUN_DIR}/var/lib/mysql-worker/mysql.sock" --user=root -pchangeme -e "DROP DATABASE qservMeta"
+    kill `cat ${QSERV_RUN_DIR}/var/run/mysqld-worker/mysqld.pid`
 
-~/qserv-run/2014_11.0/bin/qserv-start.sh
-qserv-check-integration.py --case=01
+    ~/qserv-run/2014_11.0/bin/qserv-start.sh
+    qserv-check-integration.py --case=${case}
+    ${QSERV_RUN_DIR}/bin/qserv-stop.sh
+done
