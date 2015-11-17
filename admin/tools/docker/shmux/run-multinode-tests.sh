835db3897917fee4322aa57b9983f281861a45a7
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2015-11-17 10:54:52 +0100

    Use Docker --net=host mode

--- admin/tools/docker/shmux/run-multinode-tests.sh
+++ admin/tools/docker/shmux/run-multinode-tests.sh
@@ -10,12 +10,12 @@ set -x
 IMAGE_MASTER="fjammes/qserv:latest_master_$XRD_MASTER"
 IMAGE_WORKER="fjammes/qserv:latest_worker_$XRD_MASTER"
 
-shmux -c "docker rm -f qserv; nohup docker run --name qserv --rm -h $MASTER \
+shmux -c "docker rm -f qserv; nohup docker run --net=host --name qserv --rm \
     -p 4040:4040 -p 1094:1094 -p 2131:2131 -p 12181:12181 -p 5012:5012 $IMAGE_MASTER \
     > qserv.out 2> qserv.err < /dev/null &" $MASTER
 
 # hostname has to be evaluated remotely 
-shmux -c 'docker rm -f qserv; nohup docker run --add-host=docker-1:172.16.1.238 --name qserv --rm -h $(hostname) \
+shmux -c 'docker rm -f qserv; nohup docker run --net=host --name qserv --rm \
     -p 1094:1094 -p 5012:5012 '$IMAGE_WORKER' \
     > qserv.out 2> qserv.err < /dev/null &' $WORKERS
 
