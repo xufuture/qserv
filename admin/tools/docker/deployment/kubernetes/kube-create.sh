39d3155cc0f9d06347c8a7924d79088a702447c5
Fabrice Jammes <fabrice.jammes@clermont.in2p3.fr>
2016-11-28 12:12:31 +0100

    Create weave network, and master pod

--- admin/tools/docker/deployment/kubernetes/kube-create.sh
+++ admin/tools/docker/deployment/kubernetes/kube-create.sh
@@ -11,7 +11,9 @@ SSH_CFG="$DIR/ssh_config"
 
 echo "Create Kubernetes cluster"
 ssh -t -F "$SSH_CFG" "$SWARM_NODE" "sudo systemctl start kubelet.service"
-JOIN_CMD=$(ssh -t -F "$SSH_CFG" "$SWARM_NODE" "sudo kubeadm init" | tail -n 1)
+OUTPUT=$(ssh -t -F "$SSH_CFG" "$SWARM_NODE" "sudo kubeadm init")
+
+JOIN_CMD=$(echo "$OUTPUT" | tail -n 1 | sed "s/\r//")
 
 # Join Kubernetes nodes:
 #   - Qserv master has index 0
@@ -19,7 +21,14 @@ JOIN_CMD=$(ssh -t -F "$SSH_CFG" "$SWARM_NODE" "sudo kubeadm init" | tail -n 1)
 for qserv_node in $MASTER $WORKERS
 do
     echo "Join $qserv_node to Kubernetes cluster"
-    echo "REMOVE ME: $JOIN_CMD"
-	ssh -t -F "$SSH_CFG" "$qserv_node" "sudo rm -rf /etc/kubernetes/* /var/lib/kubelet/*"
+    # FIXME: cleanup below is a workaroung, open ticket on kubernetes github
+	ssh -t -F "$SSH_CFG" "$qserv_node" "sudo systemctl stop kubelet && \
+       sudo systemctl stop docker && \
+       sudo rm -rf /etc/kubernetes/* /var/lib/kubelet/pods \
+           /var/lib/kubelet/plugins && \
+       sudo systemctl start docker && \
+       sudo systemctl start kubelet"
 	ssh -t -F "$SSH_CFG" "$qserv_node" "sudo $JOIN_CMD"
 done
+
+ssh -t -F "$SSH_CFG" "$SWARM_NODE" "kubectl apply -f https://git.io/weave-kube"
