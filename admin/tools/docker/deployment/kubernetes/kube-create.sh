3d21d6cb0e2f7d34774ae5f0bce91801c0dfe1e8
Fabrice Jammes <fabrice.jammes@clermont.in2p3.fr>
2016-11-28 18:39:47 +0100

    Add workaround to kube destroy
    umount kubelet resources

--- admin/tools/docker/deployment/kubernetes/kube-create.sh
+++ admin/tools/docker/deployment/kubernetes/kube-create.sh
@@ -4,6 +4,9 @@
 
 # @author Fabrice Jammes SLAC/IN2P3
 
+set -e
+set -x
+
 DIR=$(cd "$(dirname "$0")"; pwd -P)
 . "$DIR/env-infrastructure.sh"
 
@@ -16,19 +19,10 @@ OUTPUT=$(ssh -t -F "$SSH_CFG" "$SWARM_NODE" "sudo kubeadm init")
 JOIN_CMD=$(echo "$OUTPUT" | tail -n 1 | sed "s/\r//")
 
 # Join Kubernetes nodes:
-#   - Qserv master has index 0
-#   - QServ workers have indexes >= 1
 for qserv_node in $MASTER $WORKERS
 do
     echo "Join $qserv_node to Kubernetes cluster"
-    # FIXME: cleanup below is a workaroung, open ticket on kubernetes github
-	ssh -t -F "$SSH_CFG" "$qserv_node" "sudo systemctl stop kubelet && \
-       sudo systemctl stop docker && \
-       sudo rm -rf /etc/kubernetes/* /var/lib/kubelet/pods \
-           /var/lib/kubelet/plugins && \
-       sudo systemctl start docker && \
-       sudo systemctl start kubelet"
-	ssh -t -F "$SSH_CFG" "$qserv_node" "sudo $JOIN_CMD"
+    ssh -t -F "$SSH_CFG" "$qserv_node" "sudo $JOIN_CMD"
 done
 
 ssh -t -F "$SSH_CFG" "$SWARM_NODE" "kubectl apply -f https://git.io/weave-kube"
